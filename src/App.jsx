de1e3d0426655799204ab93b7626fb8e95032a68
Roberto Castillo <roberto.castnj@gmail.com>
2025-09-26 15:23:28 -0600

    Cargue estilos e interfaz

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Contacts from './contacts'
 
 export default function App() {
-  return <div className="bg-red-500">App</div>
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const response = await fetch('/usuarios.json')
+        if (!response.ok) {
+          throw new Error('Error al cargar contactos')
+        }
+        const data = await response.json()
+        setContacts(data)
+        console.log(contacts)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchContacts()
+  }, [])
+
+  return (
+    <div class="container">
+      <h1>Lista de Contactos</h1>
+      <ul class="contact-list">
+        <Contacts />
+        <Contacts />
+      </ul>
+      <button id="addContactBtn" class="addContactBtn">
+        +
+      </button>
+    </div>
+  )
 }
---
+++ src/contacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Contacts() {
+  return (
+    <li class="contact">
+      <div>
+        <span class="name">Juan Perez</span>
+        <span class="details">juan@example.com - 123-456-7890</span>
+      </div>
+      <button>del</button>
+    </li>
+  )
+}
